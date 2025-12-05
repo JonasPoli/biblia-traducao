282529fb418c7bc6ff84dadd0832b6e1d08cf133
Jonas Ernesto Poli <jonaspoli@Jonass-MacBook-Air.local>
2025-12-05 00:03:30 -0300

    Epub

--- src/Controller/admin/BibleController.php
+++ src/Controller/admin/BibleController.php
@@ -10,6 +10,7 @@ use App\Repository\VerseReferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use ZipArchive;
 
 final class BibleController extends AbstractController
 {
@@ -514,6 +515,264 @@ EOT;
             'Content-Type' => 'application/x-latex',
             'Content-Disposition' => 'attachment; filename="' . $book->getName() . '.tex"',
         ]);
+    }
+
+    #[Route('/admin/bible/epub/{bookId}', name: 'app_bible_download_epub', methods: ['GET'])]
+    public function downloadEpub(
+        int $bookId,
+        BookRepository $bookRepository,
+        VerseRepository $verseRepository,
+        VerseTextRepository $verseTextRepository,
+        GlobalReferenceRepository $globalReferenceRepository,
+        VerseReferenceRepository $verseReferenceRepository
+    ): Response {
+        $book = $bookRepository->find($bookId);
+        if (!$book) {
+            throw $this->createNotFoundException('Book not found');
+        }
+
+        $verses = $verseRepository->findBy(['book' => $book], ['chapter' => 'ASC', 'verse' => 'ASC']);
+        $verseTexts = $verseTextRepository->findByVersionAndBook(self::TARGET_VERSION_ID, $bookId);
+        $textsByVerseId = [];
+        foreach ($verseTexts as $vt) {
+            $textsByVerseId[$vt->getVerse()->getId()] = $vt;
+        }
+
+        $globalReferences = $globalReferenceRepository->findAll();
+        $specificReferences = $verseReferenceRepository->findByBook($bookId);
+
+        $tempFile = tempnam(sys_get_temp_dir(), 'epub');
+        $zip = new ZipArchive();
+        $zip->open($tempFile, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+        $zip->addFromString('mimetype', 'application/epub+zip');
+        $zip->setCompressionName('mimetype', ZipArchive::CM_STORE);
+
+        $containerXml = '<?xml version="1.0" encoding="UTF-8" ?>
+<container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
+    <rootfiles>
+        <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
+    </rootfiles>
+</container>';
+        $zip->addFromString('META-INF/container.xml', $containerXml);
+
+        $css = 'body { font-family: serif; line-height: 1.5; margin: 5%; }
+h1 { text-align: center; margin-bottom: 2em; }
+h2 { text-align: center; margin-top: 1em; margin-bottom: 1em; display: none; } /* Hide chapter number header as it is in drop cap */
+.chapter-number { font-size: 3em; float: left; margin-right: 0.1em; line-height: 0.8; font-weight: bold; }
+.verse-number { font-size: 0.75em; vertical-align: super; font-weight: bold; margin-right: 0.2em; }
+.subject { text-align: center; font-style: italic; margin: 1em 0; font-size: 0.9em; display: block; width: 100%; }
+.verse { margin-bottom: 0.5em; text-align: justify; }
+a.footnote-link { vertical-align: super; font-size: 0.75em; text-decoration: none; color: inherit; }
+.footnotes { margin-top: 2em; border-top: 1px solid #ccc; padding-top: 1em; }
+.footnote-item { font-size: 0.9em; margin-bottom: 0.5em; }
+.footnote-back { text-decoration: none; color: inherit; }';
+        $zip->addFromString('OEBPS/styles.css', $css);
+
+        // Organize data
+        $chapters = [];
+        foreach ($verses as $verse) {
+            $chapters[$verse->getChapter()][] = $verse;
+        }
+
+        $manifestItems = '<item id="styles" href="styles.css" media-type="text/css"/>' . "\n";
+        $spineItems = '';
+        $navPoints = '';
+        
+        // Generate Chapters
+        foreach ($chapters as $chapterNum => $chapterVerses) {
+            $chapterId = "chapter_{$chapterNum}";
+            $filename = "{$chapterId}.xhtml";
+            
+            $manifestItems .= '<item id="' . $chapterId . '" href="' . $filename . '" media-type="application/xhtml+xml"/>' . "\n";
+            $spineItems .= '<itemref idref="' . $chapterId . '"/>' . "\n";
+            $navPoints .= '<navPoint id="navPoint-' . $chapterNum . '" playOrder="' . $chapterNum . '">
+                <navLabel><text>Chapter ' . $chapterNum . '</text></navLabel>
+                <content src="' . $filename . '"/>
+            </navPoint>' . "\n";
+
+            $content = '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+<head>
+    <title>' . $book->getName() . ' ' . $chapterNum . '</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>';
+            
+            // Only show Book Title on first chapter
+            if ($chapterNum == 1) {
+                $content .= '<h1>' . htmlspecialchars($book->getName()) . '</h1>';
+            }
+
+            $footnotesHtml = '';
+            $refCounter = 1;
+            $chapterGenerated = false;
+
+            foreach ($chapterVerses as $index => $verse) {
+                $vt = $textsByVerseId[$verse->getId()] ?? null;
+                $text = $vt ? $vt->getText() : '';
+                $text = html_entity_decode($text, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+                $text = strip_tags($text);
+
+                // Handle Subject
+                $title = $vt ? $vt->getTitle() : null;
+                if ($title) {
+                    $decodedTitle = html_entity_decode($title, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+                    $content .= '<div class="subject">' . htmlspecialchars($decodedTitle) . '</div>';
+                }
+
+                // Process References (Reuse logic from downloadLatex but adapted for HTML)
+                $verseRefs = [];
+                $seenTermsExact = [];
+                
+                // 1. Specific References
+                foreach ($specificReferences as $sr) {
+                    if ($sr->getVerse()->getId() !== $verse->getId()) continue;
+                    $term = trim($sr->getTerm() ?: '');
+                    if (!$term) continue;
+                    $termNormalized = preg_replace('/\s+/', ' ', strtolower($term));
+                    if (isset($seenTermsExact[$termNormalized])) continue;
+                    
+                    $refText = html_entity_decode($sr->getReferenceText(), ENT_QUOTES | ENT_HTML5, 'UTF-8');
+                    $refText = strip_tags($refText);
+                    $verseRefs[] = ['term' => $term, 'text' => $refText];
+                    $seenTermsExact[$termNormalized] = true;
+                }
+
+                // 2. Global References
+                foreach ($globalReferences as $gr) {
+                    $term = trim($gr->getTerm() ?: '');
+                    if (!$term) continue;
+                    $termNormalized = preg_replace('/\s+/', ' ', strtolower($term));
+                    if (isset($seenTermsExact[$termNormalized])) continue;
+                    
+                    if (stripos($text, $term) !== false) {
+                        $refText = html_entity_decode($gr->getReferenceText(), ENT_QUOTES | ENT_HTML5, 'UTF-8');
+                        $refText = strip_tags($refText);
+                        $verseRefs[] = ['term' => $term, 'text' => $refText];
+                        $seenTermsExact[$termNormalized] = true;
+                    }
+                }
+
+                // Sort and Dedup (simplified)
+                $refsWithPos = [];
+                foreach ($verseRefs as $ref) {
+                    $term = $ref['term'];
+                    $pos = 0;
+                    if ($term) {
+                        $foundPos = stripos($text, $term);
+                        $pos = ($foundPos !== false) ? $foundPos : 0;
+                    }
+                    $refsWithPos[] = ['pos' => $pos, 'ref' => $ref];
+                }
+                usort($refsWithPos, fn($a, $b) => $a['pos'] <=> $b['pos']);
+
+                // Inject References
+                $processedText = $text;
+                $offset = 0;
+                $placeholders = [];
+                
+                foreach ($refsWithPos as $item) {
+                    $refId = $refCounter++;
+                    $pos = $item['pos'];
+                    $ref = $item['ref'];
+                    
+                    $placeholder = "[[REF_{$refId}]]";
+                    $noteId = "note-{$chapterNum}-{$refId}";
+                    $refLinkId = "ref-{$chapterNum}-{$refId}";
+                    
+                    // HTML Link
+                    $linkHtml = sprintf(
+                        '<a href="#%s" id="%s" class="footnote-link" epub:type="noteref">[%d]</a>',
+                        $noteId,
+                        $refLinkId,
+                        $refId
+                    );
+                    
+                    $placeholders[$placeholder] = $linkHtml;
+                    
+                    // Add to footnotes
+                    $verseRefText = sprintf("%d:%d", $verse->getChapter(), $verse->getVerse());
+                    $footnotesHtml .= sprintf(
+                        '<div class="footnote-item" id="%s"><a href="#%s" class="footnote-back">^</a> <strong>%s</strong> %s</div>',
+                        $noteId,
+                        $refLinkId,
+                        $verseRefText,
+                        htmlspecialchars($ref['text'])
+                    );
+
+                    $adjustedPos = $pos + $offset;
+                    $processedText = substr_replace($processedText, $placeholder, $adjustedPos, 0);
+                    $offset += strlen($placeholder);
+                }
+
+                $finalText = htmlspecialchars($processedText);
+                foreach ($placeholders as $ph => $html) {
+                    $finalText = str_replace(htmlspecialchars($ph), $html, $finalText);
+                }
+
+                // Verse Output
+                $content .= '<p class="verse">';
+                if ($verse->getVerse() == 1) {
+                    $content .= '<span class="chapter-number">' . $chapterNum . '</span>';
+                } else {
+                    $content .= '<span class="verse-number">' . $verse->getVerse() . '</span>';
+                }
+                $content .= $finalText . '</p>';
+            }
+
+            if (!empty($footnotesHtml)) {
+                $content .= '<div class="footnotes" epub:type="footnotes">' . $footnotesHtml . '</div>';
+            }
+
+            $content .= '</body></html>';
+            $zip->addFromString('OEBPS/' . $filename, $content);
+        }
+
+        // Content.opf
+        $opf = '<?xml version="1.0" encoding="UTF-8"?>
+<package xmlns="http://www.idpf.org/2007/opf" unique-identifier="BookId" version="3.0">
+    <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
+        <dc:title>' . htmlspecialchars($book->getName()) . '</dc:title>
+        <dc:language>pt-BR</dc:language>
+        <dc:identifier id="BookId">urn:uuid:' . uniqid() . '</dc:identifier>
+    </metadata>
+    <manifest>
+        <item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
+        ' . $manifestItems . '
+    </manifest>
+    <spine toc="ncx">
+        ' . $spineItems . '
+    </spine>
+</package>';
+        $zip->addFromString('OEBPS/content.opf', $opf);
+
+        // TOC.ncx
+        $ncx = '<?xml version="1.0" encoding="UTF-8"?>
+<ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
+    <head>
+        <meta name="dtb:uid" content="urn:uuid:12345"/>
+        <meta name="dtb:depth" content="1"/>
+        <meta name="dtb:totalPageCount" content="0"/>
+        <meta name="dtb:maxPageNumber" content="0"/>
+    </head>
+    <docTitle><text>' . htmlspecialchars($book->getName()) . '</text></docTitle>
+    <navMap>
+        ' . $navPoints . '
+    </navMap>
+</ncx>';
+        $zip->addFromString('OEBPS/toc.ncx', $ncx);
+
+        $zip->close();
+
+        $response = new Response(file_get_contents($tempFile));
+        $response->headers->set('Content-Type', 'application/epub+zip');
+        $response->headers->set('Content-Disposition', 'attachment; filename="' . $book->getName() . '.epub"');
+        
+        unlink($tempFile);
+        
+        return $response;
     }
 
     private function escapeLatex(string $text): string
